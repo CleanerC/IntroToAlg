7d440d5a09f08594f7d2f1476b351529f27fb541
Jiawei Xiang <xjw031201@gmail.com>
2023-10-21 22:01:24 -0400

    HW4

---
+++ HW4/SkipList.h
@@ -0,0 +1,111 @@
+//
+//  SkipList.h
+//  Skip_List
+//
+//  Copyright © Tali Moreshet. All rights reserved.
+//
+//
+//  This is an implementation of a Skip List class, consisting of Linked Lists, which are made up of Nodes.
+//  All classes are templated, such that their types may be provided at creation.
+//  Nodes contain data (which doubles as key), and pointers to all directions.
+
+#ifndef SkipList_h
+#define SkipList_h
+
+#include <iostream>
+#include <cstdlib>
+#include <stdlib.h>
+
+using namespace std;
+
+template<class T>
+class Node {
+public:
+    Node(const T& data);                            // constructor
+    T data;                                         // node data, doubles as key
+    Node<T> *next;                                  // node next pointer
+    Node<T> *prev;                                  // node previous pointer
+    Node<T> *up;                                    // node above pointer
+    Node<T> *down;                                  // node below pointer
+    void printData();                               // prints data value
+    void print();                                   // prints entire node object content
+};
+
+template <class T>
+class LinkedList {
+public:
+    LinkedList(T minVal, T maxVal);                 // constructor: accepts -infinity, +infinity values,
+                                                    //  creates linked list with two corresponding nodes
+    ~LinkedList();                                  // destructor
+    Node<T>* search(Node<T>* location, T data);     // search for data starting at location, return node <= data
+    Node<T>* insert(Node<T>* location, T data);     // insert node with data at new node to be placed directly 
+                                                    //  after node with povided location, return new node
+    void printData();                               // prints linked list data
+    void print();                                   // prints linked list with all nodes pointers
+    Node<T>* head;                                  // head of list
+};
+
+template <class T>
+class SkipList {
+public:
+    SkipList(T minVal, T maxVal);                   // constructor: accepts -infinity, +infinity values,
+                                                    //  creates skip list with top-level only
+    ~SkipList();                                    // destructor
+    Node<T>* search(T data);                        // search for data, return node <= data (existing node, or location
+                                                    //  where new node should be inserted in the bottom-most list)
+    Node<T>* insert(T data);                            // insert node with data, return pointer if inserted, NULL if error
+    void printData();                               // prints skip list data
+    void print();                                   // prints skip list with all nodes pointers
+    LinkedList<T> *topList;                         // pointer to the top-most list
+    int randSeed = 330;                             // to be used as seed for the getRand() function
+};
+
+//returns 0 or 1 with a 50% chance 
+//When it returns 1, insert the node to next level of the skiplist
+int getRand(){
+    return rand()%2;
+}
+
+/********************** From here down is the content of the LinkedList.cpp file: ***********************/
+
+/****** Implementation of Node ******/
+
+// Constructor
+template<class T>
+Node<T>::Node(const T& data)
+{
+    this->data = data;
+    this->next = nullptr;
+    this->prev = nullptr;
+    this->up = nullptr;
+    this->down = nullptr;
+}
+
+// Print node's data value
+template <class T>
+void Node<T>::printData()
+{
+    cout << data << " ";
+}
+
+// Print entire node object content (data value, object's address, and all pointers)
+template <class T>
+void Node<T>::print()
+{
+    cout << " * " << data << ": [addr: " << this << " next: " << this->next << " prev: " << this->prev << " up: " << this->up << " down: " << this->down << "]  ";
+}
+
+
+/****** Implementation of linked list ******/
+
+/*** TO BE COMPLETED ***/
+
+
+
+/****** Skip List Implementation ******/
+
+   /*** TO BE COMPLETED ***/
+
+
+
+#endif /* SkipList_h */
---
+++ HW4/main.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  Skip_List
+//
+//  Copyright © Tali Moreshet. All rights reserved.
+//
+
+#include <iostream>
+#include <limits.h>
+#include "SkipList.h"
+
+int main(int argc, const char * argv[]) {
+
+  /* Uncomment */
+  /*  SkipList<int> *mySL= new SkipList<int>(INT_MIN, INT_MAX);
+    
+  mySL->insert(2);
+  mySL->insert(1);
+  mySL->insert(7);
+  mySL->insert(5);
+  mySL->insert(9);
+  mySL->insert(6);
+  mySL->insert(3);
+    
+  mySL->insert(7);
+    
+  cout << "SkipList data: \n";
+  mySL->printData();
+    
+  cout << "\n SkipList entire content: \n";
+  mySL->print();
+  delete mySL;
+  */
+
+    return 0;
+}
