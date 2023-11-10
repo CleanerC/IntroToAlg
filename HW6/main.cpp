ad2968c9f3d7df7279e669059780234bb53d93a9
Jiawei Xiang <xjw031201@gmail.com>
2023-11-10 15:30:43 -0500

    HW6

---
+++ HW6/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Problem5.h"
+
+using namespace std;
+
+int main()
+{
+    // cout << "The number of BU-IDs whose digits add to a number greater than 28: " << fiveA() << endl;
+    cout << "Number of valid dictionary words that don't end with last letter of last name: " << fiveB() << endl;
+    // cout << "Longest palindrome: " << fiveC() << endl;
+}
