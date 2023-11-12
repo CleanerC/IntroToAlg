26d4ffed12a4fa0c98fd670c532fc1f9b36e16b2
Jiawei Xiang <xjw031201@gmail.com>
2023-11-11 21:50:09 -0500

    modified

--- HW6/main.cpp
+++ HW6/main.cpp
@@ -6,6 +6,6 @@ using namespace std;
 int main()
 {
     // cout << "The number of BU-IDs whose digits add to a number greater than 28: " << fiveA() << endl;
-    cout << "Number of valid dictionary words that don't end with last letter of last name: " << fiveB() << endl;
-    // cout << "Longest palindrome: " << fiveC() << endl;
+    // cout << "Number of valid dictionary words that don't end with last letter of last name: " << fiveB() << endl;
+    cout << "Longest palindrome: " << fiveC() << endl;
 }
